d710c19993e8daa563e0994c21d72a5e5a6fab75
huazai <who.seek.me@java98k.vip>
2019-05-01 18:17:43 +0800

    新增员工 Service 接口类

---
+++ aiyou-manager-service/src/main/java/com/huazai/b2c/aiyou/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.huazai.b2c.aiyou.service;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description TODO
+ *              </ul>
+ * @className EmployeeService
+ * @package com.huazai.b2c.aiyou.service
+ * @createdTime 2019年5月1日 下午6:16:03
+ *
+ * @version V1.0.0
+ */
+public class EmployeeService
+{
+
+}
